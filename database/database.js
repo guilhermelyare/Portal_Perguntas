b44cc3bd9a5f02cdd7232cef8177a20c0394a11f
luizcarlosom <lcom@discente.ifpe.edu.br>
2024-03-04 10:40:59 -0300

    refactor: Usuário alterado

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Squelize = require('sequelize') // squeliza faz com que não precise usar comandos MySQL para usar o banco de dados, em vez disso usa-se comandos do próprio JS
 
-const connection = new Squelize('guiaperguntas','root','0gZHEdhY@',{
+const connection = new Squelize('guiaperguntas','root','123456',{
     host : 'localhost',
     dialect: 'mysql'
 });
