d14d632bdfb90c23ffbd9451b8017e738c0c33d7
NOTE-MT19\cgti <guilhermelyare@gmail.com>
2024-03-06 11:00:47 -0300

    feat: new database password

--- database/database.js
+++ database/database.js
@@ -1,6 +1,6 @@
 const Squelize = require('sequelize') // squeliza faz com que não precise usar comandos MySQL para usar o banco de dados, em vez disso usa-se comandos do próprio JS
 
-const connection = new Squelize('guiaperguntas','root','29112000',{
+const connection = new Squelize('guiaperguntas','root','12345678',{
     host : 'localhost',
     dialect: 'mysql'
 });
